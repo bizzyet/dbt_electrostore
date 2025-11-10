47b93e8283c91d7123cf350e2591873f994a6de6
Eirik T <eirik@steigland.com>
2025-11-10 22:42:49 +0100

    add docs and workflow changes

---
+++ tests_sql.sql
@@ -0,0 +1,38 @@
+--insert bad row
+  INSERT INTO [little-debby].[archive_electrostore].[sales] (
+      sale_id,
+      customer_id,
+      product_id,
+      store_id,
+      sale_date,
+      quantity,
+      unit_price,
+      discount_percent,
+      total_amount,
+      _dlt_load_id,
+      _dlt_id
+  )
+  SELECT TOP 1
+      999999 AS sale_id,                    -- Random non-existent sale_id
+      88888 AS customer_id,                 -- Non-existent customer (will ERROR)
+      77777 AS product_id,                  -- Non-existent product (will WARN)
+      66666 AS store_id,                    -- Non-existent store (will ERROR)
+      sale_date,
+      quantity,
+      unit_price,
+      discount_percent,
+      total_amount,
+      'test_load' AS _dlt_load_id,
+      'test_id_sales' AS _dlt_id
+  FROM [little-debby].[archive_electrostore].[sales];
+
+
+--check 
+  SELECT * FROM [little-debby].[archive_electrostore].[sales] WHERE sale_id = 999999;
+
+--clean up
+  DELETE FROM [little-debby].[archive_electrostore].[sales] WHERE sale_id = 999999;
+
+
+
+
